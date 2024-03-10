e4b57f1dbf68d708ab60403bdf6d9a510445b93a
Sid McLaughlin <sid.mclaughlin@gov.bc.ca>
2024-03-10 13:21:58 -0700

    add app docker info

--- apps/app/vite.config.ts
+++ apps/app/vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  envDir: path.resolve(import.meta.dirname, '..', '..'),
   plugins: [react()],
 });
